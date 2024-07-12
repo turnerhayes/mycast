3e161973e6b30cfe9d0a618f0fdadf249c45bbc2
Turner Hayes <dev@turnerhayes.com>
2024-07-12 16:18:50 +0200

    Begin drag-and-drop development for playlist

--- app/components/Playlist/Playlist.tsx
+++ app/components/Playlist/Playlist.tsx
@@ -7,11 +7,92 @@ import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { useAppSelector } from "@/lib/redux/hooks";
 import { getDefaultPlaylist, getPodcastEpisodes } from "@/lib/redux/selectors";
 import { Playlist } from "@/app/playlist";
 import { EpisodeLink, PodcastLink } from "@/app/components/Links";
+import { Podcast, PodcastEpisode } from "@/app/podcast";
+import { useTheme } from "@mui/material";
 
+
+const DRAG_ITEM_TYPE = "playlist_item";
+
+const PlaylistItem = (
+    {
+        index,
+        podcast,
+        episode,
+    }: {
+        index: number;
+        podcast: Podcast;
+        episode: PodcastEpisode;
+    }
+) => {
+    const theme = useTheme();
+    const [{isDragging}, drag] = useDrag(() => ({
+        type: DRAG_ITEM_TYPE,
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    }));
+
+    const [{isOver}, drop] = useDrop(() => ({
+        accept: DRAG_ITEM_TYPE,
+        drop: (item, monitor) => {
+            console.log("item:", item);
+            console.log("monitor:", monitor);
+        },
+        collect: (monitor) => {
+            return {
+                isOver: monitor.isOver(),
+            };
+        },
+    }));
+    
+    return (
+        <ListItem
+            ref={drag}
+            disableGutters
+            sx={{
+                backgroundColor: isDragging ? theme.palette.action.selected : undefined,
+                outline: isOver ? `1px solid blue` : undefined,
+            }}
+            secondaryAction={
+                <DragHandleIcon
+                    sx={{
+                        cursor: "grab",
+                    }}
+                />
+            }
+        >
+            <Typography
+                sx={{
+                    marginRight: 2,
+                }}
+            >
+                {index + 1}. 
+            </Typography>
+            
+            <ListItemText
+            >
+                <PodcastLink
+                    podcastId={podcast.id}
+                >
+                    {podcast.title}
+                </PodcastLink>
+                &nbsp; &mdash; &nbsp;
+                <EpisodeLink
+                    podcastId={podcast.id}
+                    episodeId={episode.id}
+                >
+                    {episode.title}
+                </EpisodeLink>
+            </ListItemText>
+        </ListItem>
+    );
+};
 
 const PlaylistDisplay = (
     {
@@ -35,52 +116,25 @@ const PlaylistDisplay = (
     }
 
     return (
-        <List
-            disablePadding
-            dense
+        <DndProvider
+            backend={HTML5Backend}
         >
-            {
-                playlist.items.map((id, index) => (
-                    <ListItem
-                        key={`${id.podcastId}>${id.episodeId}`}
-                        disableGutters
-                        secondaryAction={
-                            <IconButton
-                            >
-                                <DragHandleIcon />
-                            </IconButton>
-                        }
-                    >
-                        <Typography
-                            sx={{
-                                marginRight: 2,
-                            }}
-                        >
-                            {index + 1}. 
-                        </Typography>
-                        
-                        <ListItemText>
-                            <PodcastLink
-                                podcastId={id.podcastId}
-                            >
-                                {
-                                    podcastEpisodes[JSON.stringify(id)]!.podcast.title
-                                }
-                            </PodcastLink>
-                            &nbsp; &mdash; &nbsp;
-                            <EpisodeLink
-                                podcastId={id.podcastId}
-                                episodeId={id.episodeId}
-                            >
-                                {
-                                    podcastEpisodes[JSON.stringify(id)]!.episode.title
-                                }
-                            </EpisodeLink>
-                        </ListItemText>
-                    </ListItem>
-                ))
-            }
-        </List>
+            <List
+                disablePadding
+                dense
+            >
+                {
+                    playlist.items.map((id, index) => (
+                        <PlaylistItem
+                            key={JSON.stringify(id)}
+                            index={index}
+                            podcast={podcastEpisodes[JSON.stringify(id)]!.podcast}
+                            episode={podcastEpisodes[JSON.stringify(id)]!.episode}
+                        />
+                    ))
+                }
+            </List>
+        </DndProvider>
     );
 };
 
